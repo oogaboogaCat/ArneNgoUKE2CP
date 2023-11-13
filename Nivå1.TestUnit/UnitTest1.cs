8645be966b5426e6bc2ec98e1017d3804aaa925f
oogaboogaCat <Arne2812@outlook.com>
2023-11-13 09:32:01 +0100

    TestCase for classes works 0 wrongs

--- Nivå1.TestUnit/UnitTest1.cs
+++ Nivå1.TestUnit/UnitTest1.cs
@@ -1,3 +1,5 @@
+using ArneNgoNivå1;
+
 namespace Nivå1.TestUnit
 {
     public class Tests
@@ -6,11 +8,25 @@ namespace Nivå1.TestUnit
         public void Setup()
         {
         }
-
-        [Test]
-        public void Test1()
+        [TestCase("Iman", 23, "DangerousClassmate", "#23,Iman, A Star Player! Description: DangerousClassmate")]
+        [TestCase("Arne",25, "DangerousCoder", "#25,Arne, A Star Player! Description: DangerousCoder")]
+        public void StarPlayerTest(string name,int number,string star, string expected)
         {
+            Star actual = new(name,number,star);
 
+            Assert.That(actual.ToString(), Is.EqualTo(expected));
         }
+        [TestCase("Tomas",200,"#200,Tomas")]
+        [TestCase("Agustina", 100,"#100,Agustina")]
+        [TestCase("Reza", 420, "#420,Reza")]
+
+        public void NormalPlayerTest(string name,int number, string expected) 
+        {
+            Player actual = new(name, number);
+
+            Assert.That(actual.ToString(), Is.EqualTo(expected));
+        
+        }
+
     }
 }
